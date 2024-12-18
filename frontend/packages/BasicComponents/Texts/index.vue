6452849ce4b964a97bb9ebe6e71462cdbfb9a4c4
guichuan.su <suguichuan2018@gmail.com>
2024-12-18 09:27:10 +0800

    修复文本组件静态值使用异常的问题

--- frontend/packages/BasicComponents/Texts/index.vue
+++ frontend/packages/BasicComponents/Texts/index.vue
@@ -50,6 +50,7 @@ export default {
       // }
     },
     // dataFormatting(config, data) {
+    //   console.log('config, data: ', config, data);
     //   // 文本数据配置原则：选择数据集则以后端返回的数据为主，否则以设置面板中标题设置为准
     //   if (config.dataSource.businessKey && config.dataSource.source === 'dataset') {
     //     config.customize.title = data && data.data && data.data.length ? data.data[0][config.dataSource.metricField] : '暂无数据'
@@ -62,8 +63,6 @@ export default {
       // 文本数据配置原则：选择数据集则以后端返回的数据为主，否则以设置面板中标题设置为准
       if (config.dataSource.businessKey && config.dataSource.source === 'dataset') {
         if (data && data.data && data.data.length) {
-          console.log('data.data : ', data.data );
-         
           let resp = data.data[0][config.dataSource.metricField];
           config.option.data = data.data;
           if (config.dataHandler) {
@@ -74,17 +73,11 @@ export default {
               console.info(e)
             }
           }
-         
-          // config.customize.title = resp;
           config.customize.title = (typeof resp === 'string') ? resp : JSON.stringify(resp);
         } else {
-          config.customize.title = '暂无数据';
+          config.customize.title = '暂无数3据';
           config.option.data = [];
         }
-      } else {
-        // 如果不是选择数据集的情况
-        config.customize.title = '暂无数据';
-        config.option.data = [];
       }
       this.domkey += 1;
       return config;
